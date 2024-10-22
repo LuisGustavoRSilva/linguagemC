a79632a6cda2c57ab20edb1c9d5a32a80649dde8
LuisGustavoRSilva <luisgustavo050705@gmail.com>
2024-10-22 17:09:49 -0300

    adição dos arquivos .c feitos em aula no git hub (22/10)

---
+++ src/Hello.c
@@ -0,0 +1,10 @@
+// importação da biblioteca para o uso de teclado e monitor
+#include <stdio.h>
+
+int main(){
+// para exibir uma mensagem na tela devemos usar
+// o comando printf(exibir formato)
+printf("Hello, World!\n");
+// informar a o compilador que o programa acabou
+return 0;
+}
---
+++ src/boasvindas.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+    // acidionar um pequeno cabeçalho
+    printf("-------------------programa mensagem-------------------------");
+    printf("\nSeja bem vindo ao nosso programa\n");
+    printf("--------------------------fim--------------------------------");
+
+return 0;
+}
